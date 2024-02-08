b6664f866faaa351426e7bda39414287c82ac1b2
Paul Elisha <ireajao@yahoo.com>
2024-02-08 07:32:49 +0100

    Updated file with extra data

--- contracts/GetFunded.sol
+++ contracts/GetFunded.sol
@@ -5,16 +5,17 @@ pragma solidity 0.8.22;
 // import "hardhat/console.sol";
 
 import "solmate/src/auth/Owned.sol";
-import {Time} "@openzeppelin/contracts/utils/types/Time.sol";
+import "@openzeppelin/contracts/utils/types/Time.sol";
 
-contract GetFunded is Owned, Time {
+contract GetFunded is Owned {
    
      address private immutable i_owner;
      uint256 public s_totalFunded;
      address[] private s_verifiers;
+     uint256 s_burnFee;
 
      struct Users {
-          string uAddress;
+          address uAddress;
           string name;
           string email;
           string id;
@@ -40,6 +41,7 @@ contract GetFunded is Owned, Time {
           bool isFunded;
           mapping (uint256 => bool) public active;
           string financials;
+          address[] investors;
      }
 
      Project[] s_projects;
@@ -55,16 +57,20 @@ contract GetFunded is Owned, Time {
      }
 
      mapping (address => User) private s_user;
-     mapping(uint256 => Project) private s_project;
-     mapping (uint256 => address) private idToUser;
+     mapping (uint256 => Project) private s_project;
+     mapping (uint256 => address) private idToUserAddress;
      mapping (address => bool) private isVerifier;
+     mapping (uint256 => uint256) private s_idToActive;
+     mapping(uint => mapping(address => uint))private s_investorsBalance;
+    `mapping(address => bool) private s_hasInvested;
 
-     error NotUser();
+     error InvalidUser();
      error NotVerifier();
      error AlreadyRegistered();
      error ProjectExists();
      error ZeroAddressUnAuthorized();
      error UnAuthorized();
+     error NotInvested();
 
      event Created (
           address indexed projectOwner,
@@ -75,6 +81,12 @@ contract GetFunded is Owned, Time {
 
      event Registered (
           address indexed user
+     );
+
+     event Funded(
+          uint256 indexed id,
+          address indexed owner,
+          uint256 indexed fundingAmount
      );
 
      constructor(address[] _verifiers, string calldata _role) {
@@ -90,24 +102,41 @@ contract GetFunded is Owned, Time {
      
      modifier onlyUser() {
           if (
-               msg.sender != s_user[msg.sender]
-          ) revert NotUser();
+               msg.sender != s_user[msg.sender].uAddress
+          ) revert InvalidUser();
+          if (
+               msg.sender == address(0);
+          ) revert InvalidUser();
           _;
      }
 
      modifier onlyVerifier(uint256 id) {
-          User storage user = s_user[idToUser[id]]
           if (
                isVerifier[idToUser[id]] = false;
           ) revert NotVerifier();
      }
 
+     modifier activeTime(uint _Id) {
+          Project storage project = s_project[id];
+        if(
+          _getCurrentTimestamp() <= (s_idToActive[_Id].duration)
+          ) revert NotActive();
+        if(
+          project.active
+        ) revert NotActive();
+        _;
+    }
+
      function  getProjects() external view returns (Project[] memory) {
           return s_projects;
      }
 
+     function getProjectById(uint256 id) public view returns (Project memory) {
+          return s_project[id];
+     }
+
      function removeProject(uint256 id) external only returns () {
-          delete s_project[id];
+          delete getProjectById(id);
      }
 
      function totalFunded() external view returns (uint256) {
@@ -116,6 +145,11 @@ contract GetFunded is Owned, Time {
 
      function _getCurrentTimestamp() internal view returns (uint48) {
           return Time.timestamp();
+     }
+
+     function getInvestors(uint256 id) external view returns (address[] memory) {
+          Project storage project = s_project[id];
+          return project.investors;
      }
 
      function setVerifier(string memory _role, uint256 id) public onlyOwner {
@@ -129,7 +163,7 @@ contract GetFunded is Owned, Time {
           User storage user = s_user[msg.sender];
 
           if (
-               msg.sender == s_user[msg.sender]
+               msg.sender == s_user[msg.sender].uAddress
           ) revert AlreadyRegistered();
 
           user.uAddress = msg.sender;
@@ -150,7 +184,7 @@ contract GetFunded is Owned, Time {
           Project storage project = s_project[s_projectId];
 
           if(
-               projectCreated[s_projectId]
+               project.active[project.id] = true;
           ) revert ProjectExists();
           if(
                msg.sender == address(0)
@@ -175,6 +209,8 @@ contract GetFunded is Owned, Time {
 
           project.active[project.id] = true;
 
+          s_projects.push(project);
+
           emit Created {
                msg.sender,
                project.title,
@@ -183,4 +219,76 @@ contract GetFunded is Owned, Time {
           }
      }
 
+     function fundProject(
+        uint _projectId
+     ) public payable onlyUser activeTime(_projectId) {
+          Project storage project = s_project[_projectId];
+
+          if(
+               _getCurrentTimestamp() > project.duration
+          ) revert NotActive();
+
+          if(
+               !hasInvested[msg.sender]
+          ) revert NotInvested();
+
+          project.fundingAmount += msg.value;
+          s_investorsBalance[_projectId][msg.sender] += msg.value;
+          project.investors.push(msg.sender);
+          s_hasInvested[msg.sender] = true;
+
+          emit Funded (
+               _projectId,
+               msg.sender,
+               msg.value
+          );
+     }
+
+     function refundInvestors(
+        uint _CampaignId
+     ) external returns (string memory) {
+          Campaign storage campaign = mapCampaign[_CampaignId];
+          require(
+            campaign.duration < block.timestamp,
+            "time e o yi pe, pada wa "
+          );
+          require(
+            campaign.campaignBalance < campaign.fundingGoal,
+            "campaign goal reached"
+          );
+          require(
+            contributorBalance[CampaignId][msg.sender] > 0,
+            "you do not have money in this campaign"
+          );
+          campaign.isActive = false;
+          string memory successMessage;
+
+          for (uint i = 0; i < campaign.contributors.length; i++) {
+            if (campaign.contributors[i] == msg.sender) {
+                address contributor = campaign.contributors[i];
+                uint userBalance = contributorBalance[_CampaignId][msg.sender];
+                payable(contributor).transfer(userBalance);
+                contributorBalance[_CampaignId][
+                    contributor
+                ] -= contributorBalance[_CampaignId][msg.sender];
+                campaign.campaignBalance -= userBalance;
+            }
+          }
+          return (successMessage = "Sorry we have to refund, goal not reached");
+     }
+
+     function payCampaignOwner(uint _CampaignId) public returns (string memory) {
+          Campaign storage campaign = mapCampaign[_CampaignId];
+          require(
+               campaign.campaignBalance >= campaign.fundingGoal,
+               "goal not reached"
+          );
+          require(campaign.isActive, "campaign is not active");
+          string memory successMessage;
+          address owner = campaign.campaignOwner;
+          payable(owner).transfer(campaign.campaignBalance);
+          campaign.campaignBalance = 0;
+          campaign.isActive = false;
+          return (successMessage = "Congratulations");
+     }
 }
